dd8953b5c1d53b5e2be76580686b566b3d00c31a
Gryfentube <gryfentube@gmail.com>
2018-08-07 16:45:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -54,7 +54,6 @@ var values = ("empty"); //empecher les soucis de values
 //event on démarrage
 bot.on('ready', () => {
     bot.user.setPresence({ game: { name: activ}});
-    var annonce = db.get(`ann`).map('annonce').value();
     console.log("Le bot est prêt");
     bot.channels.get(consauleYAC).send({embed: {color: 0x3ac400, author: {name: "Je suis en ligne :D",
       icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}})
