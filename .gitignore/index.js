ee1b156c2433009694d50184c702ee5a885af5b7
Gryfentube <gryfentube@gmail.com>
2018-08-10 04:13:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -48,7 +48,7 @@ function attachIsImage(msgAttach) {
 
 
 var prefix = ("_"); //définir le prefix du bot
-var activ = ("être un artiste sans talent"); //modifier la valeur entre guillemets pour changer son état au démarage
+var activ = ("esssayer d'avoir du talent"); //modifier la valeur entre guillemets pour changer son état au démarage
 var values = ("empty"); //empecher les soucis de values
 
 //event on démarrage
